509c79de05396a155475ab4240c15d3e733af992
Guilherme Meister Correa <88345985+gmeister18@users.noreply.github.com>
2024-04-29 14:27:46 -0300

    Update zumbi.cpp

--- trabalho_01/zumbi.cpp
+++ trabalho_01/zumbi.cpp
@@ -1,1 +1,38 @@
-.
+int count = 0;
+void intHandler(int dummy);
+int main(){
+    signal(SIGUSR1, intHandler); //DUMMY 10
+    signal(SIGUSR2, intHandler); //DUMMY 12
+    while(1){
+    }
+    return 0;
+}
+void intHandler(int dummy){
+    if(dummy == 10){
+        if(count == 2){
+            exit(0);
+        }
+        if(count == 1){
+            count++;
+            wait(NULL);
+        }
+        if(count == 0){
+            count++;
+            if(fork() == 0) exit(0);
+        }
+    }
+
+    if(dummy == 12){
+        if(count == 2){
+            exit(0);
+        }
+        if(count == 1){
+            count++;
+            wait(NULL);
+        }
+        if(count == 0){
+            count++;
+            if(fork() == 0) exit(0);
+        }
+    }
+}
